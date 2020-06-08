d936c8985144c5b2006296a008daf8e5d4e27bcb
Justin Yoo <justin.yoojh@gmail.com>
2020-06-08 11:17:15 +0900

    Npm style app (#1)
    * Add npm style components

---
+++ BlazorNpmSample/JsLibraries/src/progressbar.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { ProgressIndicator } from '@fluentui/react/lib/ProgressIndicator';
+
+export function renderProgressBar(count) {
+    const Progress = () => React.createElement(
+        ProgressIndicator,
+        {
+            'label': 'React Counter',
+            'description': count,
+            'percentComplete': (count % 10) * 0.1
+        },
+        null
+    );
+
+    ReactDOM.render(Progress(), document.getElementById('reactProgressBar'));
+}
